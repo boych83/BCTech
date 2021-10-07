a7defc6215f281c24142f900c00b9e028ab94f2d
Bojan Licen <bojan@licen.si>
2021-10-07 20:50:25 +0200

    xcxc

--- samples/AdminCenterApi/PowerShell/Authenticate.ps1
+++ samples/AdminCenterApi/PowerShell/Authenticate.ps1
@@ -3,15 +3,18 @@
 
 
 # Parameters
-$aadAppId = "a19cb26a-2e4c-408b-82e1-6311742ecc50"        # partner's AAD app id
+$aadAppId = "a4ed4da1-6067-44ac-8501-5be9845adc44"        # partner's AAD app id
 $aadAppRedirectUri = "http://localhost"                   # partner's AAD app redirect URI
-$aadTenantId = "8c8dbccd-c171-4937-a134-e3c5a5dd0470"     # customer's tenant id
+$aadTenantId = "e359e991-afd5-488d-ac0a-5d2bd1df85f2"     # customer's tenant id
+
+# Load Microsoft.IdentityModel.Clients.ActiveDirectory.dll
+# Add-Type -Path "C:\Users\bojan.licen\Documents\WindowsPowerShell\Modules\AzureAD\2.0.2.130\Microsoft.IdentityModel.Clients.ActiveDirectory.dll"  # Install-Module AzureAD to get this
 
 
 # Load Microsoft.Identity.Client.dll
 # Install-Package -Name Microsoft.Identity.Client -Source nuget.org -ProviderName nuget -SkipDependencies -Destination .\lib # run this line once to download the DLL
-Add-Type -Path ".\lib\Microsoft.Identity.Client.4.36.0\lib\net461\Microsoft.Identity.Client.dll"
-#Add-Type -Path ".\lib\Microsoft.Identity.Client.4.36.0\lib\netcoreapp2.1\Microsoft.Identity.Client.dll" # enable this line instead if you use PowerShell Core (pwsh)
+Add-Type -Path ".\lib\Microsoft.Identity.Client.4.21.0\lib\net461\Microsoft.Identity.Client.dll"
+#Add-Type -Path ".\lib\Microsoft.Identity.Client.4.36.0\lib\Microsoft.Identity.Client.dll" # enable this line instead if you use PowerShell Core (pwsh)
 
 # Get access token
 $clientApplication = [Microsoft.Identity.Client.PublicClientApplicationBuilder]::Create($aadAppId).WithAuthority("https://login.microsoftonline.com/$aadTenantId").WithRedirectUri($aadAppRedirectUri).Build()
@@ -26,3 +29,17 @@ $middlePartDecoded = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase6
 $middlePartDecodedPretty = (ConvertTo-Json (ConvertFrom-Json $middlePartDecoded))
 Write-Host "Contents of the access token:"
 Write-Host $middlePartDecodedPretty
+
+
+
+
+# Shared Parameters
+#$accessToken = "" # get this from the Authenticate sample
+
+
+# Get list of environments
+$response = Invoke-WebRequest `
+    -Method Get `
+    -Uri    "https://api.businesscentral.dynamics.com/admin/v2.3/applications/businesscentral/environments" `
+    -Headers @{Authorization=("Bearer $accessToken")}
+Write-Host (ConvertTo-Json (ConvertFrom-Json $response.Content))
